60442b265d1ba24aed3b9ca3b82cd105cb904c54
pj <pj@evolutio.hr>
2022-03-24 17:52:43 +0100

    edited match cell

--- lib/leagues/match_cell.dart
+++ lib/leagues/match_cell.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tablebets_proto/leagues/DataSource.dart';
 
 class MatchCell extends StatelessWidget {
-  const MatchCell({Key? key}) : super(key: key);
+  final Matches team;
+  const MatchCell({Key? key, required this.team}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +16,8 @@ class MatchCell extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Row(children: [Text('Leipzig')]),
-              Row(children: [Text('Hertha')]),
+              Row(children: [Text(team.homeTeam ?? "")]),
+              Row(children: [Text(team.awayTeam ?? "")]),
               Row(children: [
                 Image.asset(
                   'assets/tipster.png',
@@ -25,22 +27,20 @@ class MatchCell extends StatelessWidget {
             ],
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(children: [
-                IconButton(
-                    onPressed: () {},
-                    iconSize: 28,
-                    icon: Image.asset('assets/image.png'))
-              ]),
-              Row(children: [Text('1: 2.5    X: 2.5    2: 2.5')]),
-            ],
-          ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(children: [
+              IconButton(
+                  onPressed: () {},
+                  iconSize: 28,
+                  icon: Image.asset('assets/image.png'))
+            ]),
+            Row(children: [Text('${team.odds?[0].title}:${team.odds?[0].value}    ${team.odds?[1].title}:${team.odds?[1].value}    ${team.odds?[2].title}:${team.odds?[2].value}')]),
+          ],
         ),
+        // Divider(color: Colors.black,),
       ],
     );
   }
