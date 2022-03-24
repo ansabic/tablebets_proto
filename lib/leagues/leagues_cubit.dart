03f3f5b767be6427f20b3ab8dd37efed4b0e0562
lmiskovic <luka22miskovic@gmail.com>
2022-03-24 15:05:52 +0100

    adds data source

--- lib/leagues/leagues_cubit.dart
+++ lib/leagues/leagues_cubit.dart
@@ -1,8 +1,19 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:tablebets_proto/leagues/DataSource.dart';
 
 part 'leagues_state.dart';
 
 class LeaguesCubit extends Cubit<LeaguesState> {
   LeaguesCubit() : super(LeaguesInitial());
+
+  Future<List<Matches>?> getMatches() async {
+    var list = DataSource.fromJson(json.decode(DataSource.json)).matches;
+    list?.forEach((element) {
+      print(element.toString());
+    });
+    return list;
+  }
 }
--- lib/leagues/leagues_screen.dart
+++ lib/leagues/leagues_screen.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tablebets_proto/leagues/leagues_cubit.dart';
 
 class LeaguesScreen extends StatelessWidget {
   const LeaguesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      top: false,
-        bottom: false,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            iconTheme: IconThemeData(
-              color: Colors.blue
-            ),
-            title: Row(
-              children: [
-                Text("Leagues"),
-                Text("BundesLiga"),
-              ],
+    return BlocProvider(
+      create: (_) => LeaguesCubit(),
+      child: BlocBuilder<LeaguesCubit, LeaguesState>(
+          builder: (blocContext, state) {
+        return SafeArea(
+          top: false,
+          bottom: false,
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              iconTheme: IconThemeData(color: Colors.blue),
+              title: Row(
+                children: [
+                  Text(BlocProvider.of<LeaguesCubit>(blocContext)
+                      .getMatches()
+                      .toString()),
+                  Text("BundesLiga"),
+                ],
+              ),
             ),
           ),
-        ),
+        );
+      }),
     );
   }
 }
