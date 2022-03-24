23b165381b9bbeffbee9f127b22bcb2e9425c3bb
lmiskovic <luka22miskovic@gmail.com>
2022-03-24 15:06:20 +0100

    minor

--- lib/leagues/leagues_cubit.dart
+++ lib/leagues/leagues_cubit.dart
@@ -10,10 +10,6 @@ class LeaguesCubit extends Cubit<LeaguesState> {
   LeaguesCubit() : super(LeaguesInitial());
 
   Future<List<Matches>?> getMatches() async {
-    var list = DataSource.fromJson(json.decode(DataSource.json)).matches;
-    list?.forEach((element) {
-      print(element.toString());
-    });
-    return list;
+    return DataSource.fromJson(json.decode(DataSource.json)).matches;
   }
 }
